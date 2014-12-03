39ea50c41f8211b64a24c424b5f7f81bc0f71219
wrj <wrjvszq@163.com>
2014-12-03 15:49:40 +0800

    提交部分信息

--- key/app_key.c
+++ key/app_key.c
@@ -9,19 +9,28 @@
 #include<sys/stat.h>
 #include<fcntl.h>
 #include<sys/ioctl.h>
+#include<linux/input.h>
 
 int main(void)
 {
     int fd;
-    int value;
+    int count = 0, i = 0;
+    struct input_event ev_key;
 
-    fd = open("/dev/210key",0);
+    fd = open("/dev/event1",0);
 
     if(fd < 0)
         printf("open file fail\n");
 
-    read(fd,&value,4);
-        printf("the key is %d\n",value);
+    while(1)
+    {
+        count = read(fd,&ev_key,sizeof(struct input_event));
+        for(i = 0; i < (int)count/sizeof( struct input_event);i++)
+        {
+            if(EV_KEY == ev_key.type)
+            printf("type:%d,code:%d,value:%d\n",ev_key.type,ev_key.code - 1,ev_key.value);
+        }
+    }
 
     close(fd);
 
